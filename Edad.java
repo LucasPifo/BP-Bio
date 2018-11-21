7f53e067373a9042efa2780885a27eac7b94dcc2
Roberto <biosinsasitema2@gmail.com>
2018-11-21 10:51:26 -0600

    Se borro una funcion principal

--- Edad.java
+++ Edad.java
@@ -5,19 +5,3 @@ foreach(DBForm('Colaborador')->WHERE('Estatus','!=','')->get() AS $colaborador){
         save($colaborador);
     }
 }
-function busca_edad($fecha_nacimiento){
-    $dia = date("d");
-    $mes = date("m");
-    $ano = date("Y");
-    $dianaz = date("d", strtotime($fecha_nacimiento));
-    $mesnaz = date("m", strtotime($fecha_nacimiento));
-    $anonaz = date("Y", strtotime($fecha_nacimiento));
-    if (($mesnaz == $mes) && ($dianaz > $dia)) {
-        $ano = ($ano - 1);
-    }
-    if ($mesnaz > $mes) {
-        $ano = ($ano - 1);
-    }
-    $edad = ($ano - $anonaz);
-    return $edad;
-}
